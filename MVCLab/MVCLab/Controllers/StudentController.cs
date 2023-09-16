e164a7de94ac385301ecea7dbe3c903175641b50
HIENLTH @ HCMUE <hyhien@gmail.com>
2023-09-16 10:13:17 +0700

    Demo Read Write JSON

---
+++ MVCLab/MVCLab/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models;
+using System.Text.Json;
+
+namespace MVCLab.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Manage(Student sinhVien, string LuuButton)
+        {
+            if (LuuButton == "Ghi file JSON")
+            {
+                //chuyển đổi object sang json
+                var jsonString = JsonSerializer.Serialize(sinhVien);
+                System.IO.File.WriteAllText(myPathFile, jsonString);
+            }
+            return View("Index");
+        }
+
+        string myPathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Student.json");
+
+        public IActionResult OpenData(string type)
+        {
+            var student = new Student();
+            if (type == "JSON")
+            {
+                var content = System.IO.File.ReadAllText(myPathFile);
+                student = JsonSerializer.Deserialize<Student>(content);
+            }
+
+            return View("Index", student);
+        }
+    }
+}
---
+++ MVCLab/MVCLab/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCLab.Models
+{
+    public class Student
+    {
+        [Display(Name ="Mã sinh viên")]
+        public string StudentId {get; set;}
+
+        [Display(Name = "Họ tên sinh viên")]
+        public string StudentName {get; set;}
+
+        [Display(Name = "Điểm")]
+        public double Mark {get; set;}
+        public bool IsMale {get; set;}
+        public string? Grade
+        {
+            get
+            {
+                if (Mark >= 8.5) return "A";
+                if (Mark >= 7.8) return "B+";
+                if (Mark >= 7) return "B";
+                if (Mark >= 5) return "C";
+                return "D";
+            }
+        }
+    }
+}
