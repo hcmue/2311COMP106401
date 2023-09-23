a0bc5d8b0a9ac2446e0c04b552cb27ae9e42128b
HIENLTH @ HCMUE <hyhien@gmail.com>
2023-09-23 08:55:45 +0700

    Upload multiple file

--- FirstProject/FirstProject/Controllers/FileUploadController.cs
+++ FirstProject/FirstProject/Controllers/FileUploadController.cs
@@ -12,24 +12,39 @@ namespace FirstProject.Controllers
         [HttpPost]
         public IActionResult HandleUploadFile(IFormFile MyFile)
         {
-            ViewBag.FileName = MyFile.FileName;
-            try
+            if (MyFile != null)
             {
-                var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Data");
-                if (!Directory.Exists(folderPath))
+                try
                 {
-                    Directory.CreateDirectory(folderPath);
+                    var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Data");
+                    if (!Directory.Exists(folderPath))
+                    {
+                        Directory.CreateDirectory(folderPath);
+                    }
+                    var filePath = Path.Combine(folderPath, MyFile.FileName);
+                    using (var file = new FileStream(filePath, FileMode.CreateNew))
+                    {
+                        MyFile.CopyTo(file);
+                    }
+                    TempData["ThongBao"] = "Upload file thành công";
                 }
-                var filePath = Path.Combine(folderPath, MyFile.FileName);
+                catch (Exception ex)
+                {
+                    TempData["ThongBao"] = $"Lỗi Upload file: {ex.Message}";
+                }
+            }
+            return Redirect("Index");
+        }
+
+        public IActionResult HandleUploadFiles(List<IFormFile> MyFiles)
+        {
+            foreach(var MyFile in MyFiles)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Data", MyFile.FileName);
                 using (var file = new FileStream(filePath, FileMode.CreateNew))
                 {
                     MyFile.CopyTo(file);
                 }
-                TempData["ThongBao"] = "Upload file thành công";
-            }
-            catch(Exception ex)
-            {
-                TempData["ThongBao"] = $"Lỗi Upload file: {ex.Message}";
             }
             return Redirect("Index");
         }
