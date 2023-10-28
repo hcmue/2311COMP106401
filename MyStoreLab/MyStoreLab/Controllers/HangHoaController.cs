ddcb200ed6813d16f3155df4e6060106e7ee9035
HIENLTH @ HCMUE <hyhien@gmail.com>
2023-10-28 10:15:02 +0700

    Query

---
+++ MyStoreLab/MyStoreLab/Controllers/HangHoaController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStoreLab.Data;
+using MyStoreLab.Models;
+
+namespace MyStoreLab.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        private readonly MyeStoreContext _ctx;
+
+        public HangHoaController(MyeStoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var data = _ctx.HangHoas
+                .Select(hh => new HangHoaVM
+                {
+                    MaHh = hh.MaHh,
+                    TenHh = hh.TenHh,
+                    DonGia = hh.DonGia ?? 0,
+                    Hinh = hh.Hinh ?? string.Empty,
+                    Loai = hh.MaLoaiNavigation.TenLoai
+                }).ToList();
+            return View(data);
+        }
+
+        public IActionResult Search(string? keyword, double? priceFrom, double? priceTo)
+        {
+            var data = _ctx.HangHoas.AsQueryable();
+
+            if (keyword != null)
+            {
+                data = data.Where(p => p.TenHh.Contains(keyword));
+            }
+            if (priceFrom != null)
+            {
+                data = data.Where(p => p.DonGia >= priceFrom);
+            }
+            if (priceTo != null)
+            {
+                data = data.Where(p => p.DonGia <= priceTo);
+            }
+
+            var result = data.Select(hh => new HangHoaVM
+            {
+                MaHh = hh.MaHh,
+                TenHh = hh.TenHh,
+                DonGia = hh.DonGia ?? 0,
+                Hinh = hh.Hinh ?? string.Empty,
+                Loai = hh.MaLoaiNavigation.TenLoai
+            }).ToList();
+            return View("Index", result);
+        }
+    }
+}
---
+++ MyStoreLab/MyStoreLab/Models/HangHoaVM.cs
@@ -0,0 +1,11 @@
+﻿namespace MyStoreLab.Models
+{
+    public class HangHoaVM
+    {
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public double DonGia { get; set; }
+        public string Loai { get; set; }
+    }
+}
