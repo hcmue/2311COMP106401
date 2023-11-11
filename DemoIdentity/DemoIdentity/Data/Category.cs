6217a6fb21c349d5db5a5a6214723334eebf2678
HIENLTH 2023 <hyhien@gmail.com>
2023-11-11 08:29:47 +0700

    Demo Identity

---
+++ DemoIdentity/DemoIdentity/Data/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoIdentity.Data
+{
+	[Table("Category")]
+	public class Category
+	{
+		[Key]
+		public int Id { get; set; }
+		[MaxLength(100)]
+		public string Name { get; set; }
+	}
+}
